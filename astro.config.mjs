2533079a641350d554ebe7eb378db6fd21d654e1
inky3 <167996791+inky3@users.noreply.github.com>
2024-12-19 21:18:35 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 export default {
-  site: 'https://inky3.github.io', // Replace 'name' with your GitHub username
+  site: 'https://inky3.github.io', // Your GitHub username
   build: {
-    // This is the important part
-    base: '/me/', // Replace 'repo-name' with your GitHub repository name
+    // Remove the base path for now
   },
 };
