9e8dcc91ee2efe7e543df67c8c7e793d482b30e3
inky3 <167996791+inky3@users.noreply.github.com>
2024-12-19 21:13:10 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  base: '/me/',
-})
+export default {
+  site: 'https://github.com/inky3/me.git', // Replace 'name' with your GitHub username
+  build: {
+    base: '/me/', // Replace 'repo-name' with your repository name
+  },
+};
